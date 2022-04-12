f2374e067b49ba7db2019a610d7ad79864e57184
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2022-04-12 19:41:43 +0900

    인증번호 발송과 검증 기능 작성

---
+++ backend/src/utils/sns.js
@@ -0,0 +1,23 @@
+const CryptoJS = require('crypto-js');
+
+// 네이버 SMS API 호출을 위해서 필요한 암호화된 서명을 생성하는 함수
+function makeSignature(timestamp, suburl, method) {
+	var space = " ";				// one space
+	var newLine = "\n";				// new line
+	var accessKey = process.env.NAVER_ACCESS_KEY;			// access key id (from portal or Sub Account)
+	var secretKey = process.env.NAVER_SECRET_KEY;			// secret key (from portal or Sub Account)
+
+	var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+	hmac.update(method);
+	hmac.update(space);
+	hmac.update(suburl);
+	hmac.update(newLine);
+	hmac.update(timestamp);
+	hmac.update(newLine);
+	hmac.update(accessKey);
+
+	var hash = hmac.finalize();
+	return hash.toString(CryptoJS.enc.Base64);
+}
+
+module.exports.makeSignature = makeSignature;
