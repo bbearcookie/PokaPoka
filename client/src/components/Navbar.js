599ccb36f92fc4f2f8ddb0d1bf2d689d58b336f0
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2022-05-27 23:53:13 +0900

    네비바에 아이콘 추가. 아이디 너무 길면 ellipsis 되도록 수정.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import classNames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotate, faBriefcase, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import useRequest from '../utils/useRequest';
 import * as api from '../utils/api';
 import AuthContext from '../contexts/Auth';
@@ -23,58 +25,69 @@ const Navbar = () => {
     } catch (err) {}
   }
 
-  return (
-    <nav className="Navbar">
-      {authState.user.role === 'admin' ? 
-      <Link className="nav-logo" to="/admin/user">PokaPoka</Link> : 
-      <Link className="nav-logo" to="/main">PokaPoka</Link>}
-      
-      <section className="nav-items">
-        {authState.user.username ?
-        <>
-          <section className="link_section">
-            {authState.user.role === 'user' ?
-            <>
-              <Link
-                className={classNames("nav-item link", {"active": URI.includes('/trade')})}
-                to="/trade/all"
-              >포토카드 교환</Link>
-              <Link
-                className={classNames("nav-item link", {"active": URI.includes('/stoarage')})}
-                to="/stoarage/permanent"
-              >포토카드 보관함</Link>
-              <Link
-                className={classNames("nav-item link", {"active": URI.includes('/notice')})}
-                to="/notice"
-              >공지사항</Link>
-            </>
-            : null}
-          </section>
-          <section className="user_section">
-            {authState.user.role === 'user' &&
-              <Link to="/mypage/userInfo">
-                <img width="50px" height="50px" src="/user.png" alt="사용자" />
-                <span className="nav-item">{authState.user.username}</span>
-              </Link>
-            }
-            {authState.user.role === 'admin' &&
+  if (authState.user.role) {
+    if (authState.user.role === 'admin') {
+      // 관리자에게 보여줄 네비바
+      return (
+        <nav className="Navbar">
+          <Link className="nav-logo" to="/admin/user">PokaPoka</Link>
+          <section className="nav-items">
+            <section className="link_section"></section>
+            <section className="user_section">
               <Link to="#">
                 <img width="50px" height="50px" src="/admin.png" alt="관리자" />
                 <span className="nav-item">{authState.user.username}</span>
               </Link>
-            }
-            <span className="nav-item logout" onClick={onClickLogout}>로그아웃</span>
+              <span className="nav-item logout" onClick={onClickLogout}>로그아웃</span>
+            </section>
           </section>
-        </> :
-        <>
+        </nav>
+      );
+    } else if (authState.user.role === 'user') {
+      // 사용자에게 보여줄 네비바
+      return (
+        <nav className="Navbar">
+          <Link className="nav-logo" to="/main">PokaPoka</Link>
+          <section className="nav-items">
+            <section className="link_section">
+              <Link
+                className={classNames("nav-item link", {"active": URI.includes('/trade')})}
+                to="/trade/all"
+              ><FontAwesomeIcon icon={faRotate} />&nbsp;&nbsp;교환</Link>
+              <Link
+                className={classNames("nav-item link", {"active": URI.includes('/stoarage')})}
+                to="/stoarage/permanent"
+              ><FontAwesomeIcon icon={faBriefcase} />&nbsp;&nbsp;보관함</Link>
+              <Link
+                className={classNames("nav-item link", {"active": URI.includes('/notice')})}
+                to="/notice"
+              ><FontAwesomeIcon icon={faInfoCircle} />&nbsp;&nbsp;공지사항</Link>
+            </section>
+            <section className="user_section">
+              <Link to="/mypage/userInfo">
+                <img width="50px" height="50px" src="/user.png" alt="사용자" />
+                <span className="nav-item">{authState.user.username}</span>
+              </Link>
+              <span className="nav-item logout" onClick={onClickLogout}>로그아웃</span>
+            </section>
+          </section>
+        </nav>
+      );
+    }
+  } else {
+    // 로그인 안한 사용자에게 보여줄 네비바
+    return (
+      <nav className="Navbar">
+        <Link className="nav-logo" to="/main">PokaPoka</Link>
+        <section className="nav-items">
           <section className="link_section"></section>
           <section className="user_section">
             <Link className="nav-item link" to="/auth/login">로그인</Link>
           </section>
-        </>}
-      </section>
-    </nav>
-  );
+        </section>
+      </nav>
+    )
+  }
 };
 
 export default Navbar;
